36f35916a4c9f126479599ae4fc64ccfd82e6716
kerrea <zhuzhenxiao000@vip.qq.com>
2017-12-13 22:44:59 +0800

    Finish round 2 programme

--- Main/DFRobot_TCS3200.h
+++ Main/DFRobot_TCS3200.h
@@ -2,6 +2,10 @@
 int LED,OE,S0,S1,S2,S3,Input;
 bool inited = false;
 bool isLED_Enabled = false;
+const int Red = 0;
+const int Green = 1;
+const int Blue = 2;
+const int Clear = 3;
 void initTCS3200(int LED_Pin,int OE_Pin,int S0_Pin,int S1_Pin,int S2_Pin,int S3_Pin,int Input_Pin){
   LED = LED_Pin;
   OE = OE_Pin;
@@ -28,6 +32,72 @@ void enableLED(bool Enable){
 
 bool getIsLED_Enabled(){
   return isLED_Enabled;
+}
+
+/**
+ * ------------------------------------
+ * Ratio integer from 0 to 3
+ * 0 Power Down
+ * 1 2%
+ * 2 20%
+ * 3 100%
+ * ------------------------------------
+ * Filter Type integer frome 0 to 3
+ * 0 Red
+ * 1 Green
+ * 2 Blue
+ * 3 Clear
+ * ------------------------------------
+ */
+void readData(int Ratio,int Type){
+  switch(Ratio){
+    case 0:
+      digitalWrite(S0_Pin,false);
+      digitalWrite(S1_Pin,false);
+    break;
+    case 1:
+      digitalWrite(S0_Pin,false);
+      digitalWrite(S1_Pin,true);
+    break;
+    case 2:
+      digitalWrite(S0_Pin,true);
+      digitalWrite(S1_Pin,false);
+    break;
+    case 3:
+      digitalWrite(S0_Pin,true);
+      digitalWrite(S1_Pin,true);
+    break;
+    default:
+      if(Serial.available() > 0){
+        Serial.println("Error Ratio: " + Ratio);
+      }
+      return;
+    break;
+  }
+  switch(Type){
+    case Red:
+      digitalWrite(S3_Pin,false);
+      digitalWrite(S3_Pin,false);
+    break;
+    case Green:
+      digitalWrite(S3_Pin,true);
+      digitalWrite(S3_Pin,true);
+    break;
+    case Blue:
+      digitalWrite(S3_Pin,false);
+      digitalWrite(S3_Pin,true);
+    break;
+    case Clear:
+      digitalWrite(S3_Pin,true);
+      digitalWrite(S3_Pin,false);
+    break;
+    default:
+      if(Serial.available() > 0){
+        Serial.println("Error Type: " + Type);
+      }
+      return;
+    break;
+  }
 }
 
 
--- Main/DFRobot_Veyron_Servo_Driver.h
+++ Main/DFRobot_Veyron_Servo_Driver.h
@@ -1,3 +1,20 @@
+#include "BasicInfo.h"
 void initServoController(){
-  
+  Serial.begin(BaudRate);
+  Serial.flush();
+  Serial.println("Serial Open success");
 }
+
+/*
+ * Position [0,100]
+ * Channel [1,24]
+ */
+void servoRun(int Channel,int Position,long duration){
+  Position = map(Position,0,100,500,2500);
+  if(duration != 0){
+    Serial.println("#" + Channel +" P");
+  }else{
+    Serial.println("#" + Channel +" P" + Position +" T" + duration);
+  }
+}
+
