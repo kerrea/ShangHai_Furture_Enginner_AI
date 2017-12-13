5e6790be138ffa8ff90caae436739107322bb503
Zhenxiao_Zhu <zhuzhenxiao000@vip.qq.com>
2017-12-13 12:51:42 +0800

    Simplify Files

--- Main/MX_850_Motor_Driver.h
+++
@@ -1,1 +0,0 @@
-// Use High Speed Motors
--- Main/Main.ino
+++ Main/Main.ino
@@ -1,7 +1,7 @@
-#include "MX_850_Motor_Driver.h"
 #include "Sound_to_data.h"
 #include "Text_to_sound.h"
 #include "Veyron_Servo_Driver.h"
+#include "DFRobot_4Channel_Motor_Controller.h"
 // all #define should put in BasicInfo.h 
 void setup(){
   
